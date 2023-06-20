6de2bd4c831248ec51bfbc1d86f50660e512eeb8
CarlosVP120 <carlosveryan4@gmail.com>
2023-06-19 19:17:49 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+"use strict";
+
+/**
+ * add Event on elements
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+};
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+/**
+ * header & back top btn show when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 80) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+};
+
+addEventOnElem(window, "scroll", headerActive);
+
+// Cursor
+
+/////////////////////////////////////////////////////////////////////////
+//// PARALLAX CONFIG
+const cursor = { x: 0, y: 0 };
+let previousTime = 0;
+
+//////////////////////////////////////////////////
+//// ON MOUSE MOVE TO GET CAMERA POSITION
+document.addEventListener(
+  "mousemove",
+  (event) => {
+    event.preventDefault();
+
+    handleCursor(event);
+  },
+  false
+);
+
+const btn = document.querySelectorAll(".a"); // the >
+const customCursor = document.querySelector(".cursor");
+
+function update(e) {
+  const span = this.querySelector("span");
+
+  if (e.type === "mouseleave") {
+    span.style.cssText = "";
+  } else {
+    const { offsetX: x, offsetY: y } = e,
+      { offsetWidth: width, offsetHeight: height } = this,
+      walk = 20,
+      xWalk = (x / width) * (walk * 2) - walk,
+      yWalk = (y / height) * (walk * 2) - walk;
+    span.style.cssText = `transform: translate(${xWalk}px, ${yWalk}px);`;
+  }
+}
+
+const handleCursor = (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  customCursor.style.cssText = `left: ${x}px; top: ${y}px;`;
+};
+
+btn.forEach((b) => b.addEventListener("mousemove", update));
+btn.forEach((b) => b.addEventListener("mouseleave", update));
